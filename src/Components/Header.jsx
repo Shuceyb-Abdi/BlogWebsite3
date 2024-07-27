0f284084def4b823cbadf91f24879863242d1c02
Shuceyb-Abdi <shuceybabdi361@gmail.com>
2024-07-28 00:16:36 +0300

    Add

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Header() {
   return (
     <div className=' flex justify-between mt-5 mx-[3rem] items-center '>
-        <h1 className=' text-[1.6rem] font-Far font-semibold '>Digital <span className='text-[#5dee7f] '> Dynamo</span></h1>
+        <h1 className=' text-[1.6rem] font-Far font-semibold '>Blog <span className='text-[#5dee7f] '> Dynamo</span></h1>
         <div className=" flex space-x-4">
           <li className=' list-none text-[1.1rem]   font-semibold hover:bg-[#5dee7f] hover:py-1 hover:px-5 hover:rounded-md hover:text-white cursor-pointer  '>Home</li>
           <li className=' list-none text-[1.1rem]   font-semibold hover:bg-[#5dee7f] hover:py-1 hover:px-5 hover:rounded-md hover:text-white cursor-pointer  '>About</li>
--- src/Components/Herosection.jsx
+++ src/Components/Herosection.jsx
@@ -1,18 +1,37 @@
 import React from 'react'
-import city from'../assets/city.jpg'
+import Blog from '../assets/Blog.jpg'
+
 export default function Herosection() {
   return (
     <div className=' flex justify-between mx-[3rem] items-center mt-[4.6rem] space-x-7 '>
        <div className=" space-y-5">
        <h1 className=' text-[2.8rem] font-Far font-semibold 
-       '>Discover the World of <span className='text-[#30aa4c] '>Traveling Blog</span></h1>
-       <p className=' text-gray-500'>Join us on a journey through the fascinating world of Traveling Blog <br />
-         Our blog offers insightful articles, expert tips, and inspiring stories
-         to help you stay informed and engaged. Whether you're a </p>
+       '>Discover the World of <span className='text-[#30aa4c] '> Blogging</span></h1>
+
+       <p className="intro-text">
+  Welcome to our Blog Website! Dive into a universe of captivating content, 
+  insightful articles,
+  and expert tips that keep you informed and inspired. 
+  <p className=''>
+  Whether you're a seasoned reader or a curious newbie, 
+  our blog offers something for everyone. 
+    </p>
+    Universe of captivating content, insightful articles,
+  and expert tips that keep you informed and inspired. 
+  Whether you're a seasoned reader or a curious newbie,
+  <p>
+  Explore topics ranging from travel and lifestyle to technology and education.
+  Join us on this exciting journey and enrich your mind with diverse perspectives and engaging stories.
+  </p>
+</p>
+
          <button className='py-2 px-5 text-white rounded-md bg-[#28a745] font-semibold  '>Explore Our Blog</button>
        </div>
        <div className="">
-        <img className=' w-[800px]  rounded-md h-[450px]  ' src={city} alt="" />
+        {/* <img className=' w-[800px]  rounded-md h-[450px]  ' src={city} alt="" /> */}
+          <img className=' w-[800px]  rounded-md h-[450px]  ' src={Blog} alt="" />
+        
+      
        </div>
     </div>
   )
