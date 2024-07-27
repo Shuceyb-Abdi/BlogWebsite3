4de89f90128010ae2e755bb5e6b15b7b635497cb
Shuceyb-Abdi <shuceybabdi361@gmail.com>
2024-07-27 19:28:37 +0300

    Add

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -5,11 +5,11 @@ export default function Header() {
     <div className=' flex justify-between mt-5 mx-[3rem] items-center '>
         <h1 className=' text-[1.6rem] font-Far font-semibold '>Digital <span className='text-[#5dee7f] '> Dynamo</span></h1>
         <div className=" flex space-x-4">
-          <li className=' list-none text-[1.1rem]   font-semibold hover:bg-[#5dee7f] hover:py-1 hover:px-5 hover:rounded-md hover:text-white  '>Home</li>
-          <li className=' list-none text-[1.1rem]   font-semibold hover:bg-[#5dee7f] hover:py-1 hover:px-5 hover:rounded-md hover:text-white  '>About</li>
-          <li className=' list-none text-[1.1rem]   font-semibold hover:bg-[#5dee7f] hover:py-1 hover:px-5 hover:rounded-md hover:text-white  '>Blog</li>
-          <li className=' list-none text-[1.1rem]   font-semibold hover:bg-[#5dee7f] hover:py-1 hover:px-5 hover:rounded-md hover:text-white  '>Categories</li>
-          <li className=' list-none text-[1.1rem]   font-semibold hover:bg-[#5dee7f] hover:py-1 hover:px-5 hover:rounded-md hover:text-white  '>Contect</li>
+          <li className=' list-none text-[1.1rem]   font-semibold hover:bg-[#5dee7f] hover:py-1 hover:px-5 hover:rounded-md hover:text-white cursor-pointer  '>Home</li>
+          <li className=' list-none text-[1.1rem]   font-semibold hover:bg-[#5dee7f] hover:py-1 hover:px-5 hover:rounded-md hover:text-white cursor-pointer  '>About</li>
+          <li className=' list-none text-[1.1rem]   font-semibold hover:bg-[#5dee7f] hover:py-1 hover:px-5 hover:rounded-md hover:text-white cursor-pointer  '>Blog</li>
+          <li className=' list-none text-[1.1rem]   font-semibold hover:bg-[#5dee7f] hover:py-1 hover:px-5 hover:rounded-md hover:text-white cursor-pointer  '>Categories</li>
+          <li className=' list-none text-[1.1rem]   font-semibold hover:bg-[#5dee7f] hover:py-1 hover:px-5 hover:rounded-md hover:text-white cursor-pointer  '>Contect</li>
 
         </div>
         <div className="">
