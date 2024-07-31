156782b88242694acfc921fb1f7d1f4b20a48659
Shuceyb-Abdi <shuceybabdi361@gmail.com>
2024-07-31 17:29:25 +0300

    Update

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -1,33 +1,38 @@
-import React from 'react'
-import img1 from'../assets/travel.avif'
-import img2 from'../assets/travelone.avif'
-import img3 from'../assets/traveltwo.avif'
+import React from 'react';
+import img1 from '../assets/travel.avif';
+import img2 from '../assets/travelone.avif';
+import img3 from '../assets/traveltwo.avif';
+
 export default function About() {
   return (
-    <div className=' flex mt-[14rem]  mx-[2.7rem] space-x-5 relative '>
-           <div className=" absolute left-[29rem] top-[-10rem] my-[3rem]  ">
-            <h1 className=' text-[3rem] font-Far   '>About <span  className=' text-[#30aa4c] '> Us</span></h1>
-           </div>
-        <div className=" space-y-7 pt-[3rem] ">
-            <h1 className=' text-[2.8rem] font-Far font-semibold '>Welcome to Blog <span className='text-[#30aa4c] '>Website</span></h1>
-            <p className=' text-gray-600'>Our blog website is built using React.js, a powerful JavaScript library for building user interfaces. We leverage the flexibility and efficiency of React to deliver a seamless and interactive experience for our readers. From dynamic content updates to a responsive design, our platform ensures that you get the best possible browsing experience whether you're on a desktop or mobile device. Join us as we continue to innovate and bring you the latest stories, tips, and adventures from around the world.
-            </p>
-                 <button className='py-2 px-5 text-white rounded-md
-                  bg-[#28a745] font-semibold  '>Join Our Journey</button>
-
+    <div className='flex flex-col lg:flex-row mt-[14rem] mx-4 lg:mx-[2.7rem] space-y-5 lg:space-y-0 lg:space-x-5 relative'>
+      <div className="absolute left-[50%] transform -translate-x-1/2 lg:left-[29rem] lg:top-[-10rem] my-[3rem] text-center lg:text-left">
+        <h1 className='text-[2rem] lg:text-[3rem] font-Far'>
+          About <span className='text-[#30aa4c]'>Us</span>
+        </h1>
+      </div>
+      <div className="space-y-7 pt-[3rem] lg:pt-0 text-center lg:text-left">
+        <h1 className='text-[2rem] lg:text-[2.8rem] font-Far font-semibold'>
+          Welcome to Blog <span className='text-[#30aa4c]'>Website</span>
+        </h1>
+        <p className='text-gray-600'>
+          Our blog website is built using React.js, a powerful JavaScript library for building user interfaces. We leverage the flexibility and efficiency of React to deliver a seamless and interactive experience for our readers. From dynamic content updates to a responsive design, our platform ensures that you get the best possible browsing experience whether you're on a desktop or mobile device. Join us as we continue to innovate and bring you the latest stories, tips, and adventures from around the world.
+        </p>
+        <button className='py-2 px-5 text-white rounded-md bg-[#28a745] font-semibold'>
+          Join Our Journey
+        </button>
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-1 gap-1">
+        <div className="row-span-1 md:row-span-2 lg:row-span-7 w-full lg:w-[300px] h-[200px] md:h-[404px] bg-blue-400 rounded-md">
+          <img className='h-full w-full object-cover object-center rounded-md' src={img1} alt="Travel 1" />
         </div>
-        <div className="grid grid-rows-3 grid-flow-col gap-1">
-            <div className="row-span-7 w-[300px] h-[404px]  bg-blue-400 rounded-md ">
-                <img className=' h-full object-fill object-center rounded-md' src={img1} alt="" />
-            </div>
-            <div className="col-span-2 w-[300px] h-[200px]  bg-blue-400 rounded-md ">
-            <img className=' h-full object-fill object-center rounded-md' src={img2} alt="" />
-            </div>
-         
-            <div className=" w-[300px] h-[200px] bg-blue-400 rounded-md ">
-            <img className=' h-full object-fill object-center rounded-md' src={img3} alt="" />
-            </div>
+        <div className="col-span-1 w-full lg:w-[300px] h-[200px] bg-blue-400 rounded-md">
+          <img className='h-full w-full object-cover object-center rounded-md' src={img2} alt="Travel 2" />
         </div>
-    </div>
-  )
+        <div className="col-span-1 w-full lg:w-[300px] h-[200px] bg-blue-400 rounded-md">
+          <img className='h-full w-full object-cover object-center rounded-md' src={img3} alt="Travel 3" />
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,21 +1,34 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from 'react';
+import { Link } from 'react-router-dom';
+
 export default function Header() {
   return (
-    <div className=' flex justify-between mt-5 mx-[3rem] items-center '>
-        <h1 className=' text-[1.6rem] font-Far font-semibold '>Blog <span className='text-[#5dee7f] '> Website</span></h1>
-        <div className=" flex space-x-4">
-          <Link className='text-[1.1rem]   font-semibold hover:bg-[#5dee7f] hover:py-1 hover:px-5 hover:rounded-md hover:text-white' to='/'>Home</Link>
-          <Link className='text-[1.1rem]   font-semibold hover:bg-[#5dee7f] hover:py-1 hover:px-5 hover:rounded-md hover:text-white' to='/blog'>Blog</Link>
-          <Link className='text-[1.1rem]   font-semibold hover:bg-[#5dee7f] hover:py-1 hover:px-5 hover:rounded-md hover:text-white' to='/about'>About</Link>
-          <Link className='text-[1.1rem]   font-semibold hover:bg-[#5dee7f] hover:py-1 hover:px-5 hover:rounded-md hover:text-white' to='/Categories'>Categories</Link>
-          <Link className='text-[1.1rem]   font-semibold hover:bg-[#5dee7f] hover:py-1 hover:px-5 hover:rounded-md hover:text-white' to='/contect'>Contect</Link>
-        
-
-        </div>
-        <div className="">
-            <button className='py-2 px-5 text-white rounded-md bg-[#28a745] font-semibold  '>Blog Website</button>
-        </div>
-    </div>
-  )
+    <div className='flex flex-wrap justify-between mt-5 mx-4 md:mx-[3rem] items-center'>
+      <h1 className='text-[1.6rem] font-Far font-semibold'>
+        Blog <span className='text-[#5dee7f]'>Website</span>
+      </h1>
+      <div className="flex flex-wrap space-x-4 mt-4 md:mt-0">
+        <Link className='text-[1.1rem] font-semibold hover:bg-[#5dee7f] hover:py-1 hover:px-5 hover:rounded-md hover:text-white' to='/'>
+          Home
+        </Link>
+        <Link className='text-[1.1rem] font-semibold hover:bg-[#5dee7f] hover:py-1 hover:px-5 hover:rounded-md hover:text-white' to='/blog'>
+          Blog
+        </Link>
+        <Link className='text-[1.1rem] font-semibold hover:bg-[#5dee7f] hover:py-1 hover:px-5 hover:rounded-md hover:text-white' to='/about'>
+          About
+        </Link>
+        <Link className='text-[1.1rem] font-semibold hover:bg-[#5dee7f] hover:py-1 hover:px-5 hover:rounded-md hover:text-white' to='/Categories'>
+          Categories
+        </Link>
+        <Link className='text-[1.1rem] font-semibold hover:bg-[#5dee7f] hover:py-1 hover:px-5 hover:rounded-md hover:text-white' to='/contect'>
+          Contect
+        </Link>
+      </div>
+      <div className="mt-4 md:mt-0">
+        <button className='py-2 px-5 text-white rounded-md bg-[#28a745] font-semibold'>
+          Blog Website
+        </button>
+      </div>
+    </div>
+  );
 }
